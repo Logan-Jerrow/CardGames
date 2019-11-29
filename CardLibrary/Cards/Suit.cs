9cdbdc204a5c28353cf289fc8e032f3b71711fa1
Logan Jerrow <2975388+Logan-Jerrow@users.noreply.github.com>
2019-11-29 00:19:43 -0800

    Restructure to dotnet 3.0  (#1)
    * Testing pull request
    
    * Added missing
    
    * Removed old files from previous .net sln

---
+++ CardLibrary/Cards/Suit.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardLibrary.Cards
+{
+    public sealed class Suit : SmartEnum<Suit>
+    {
+        internal static readonly Suit Spades = new Suit(nameof(Spades), 1);
+        internal static readonly Suit Hearts = new Suit(nameof(Hearts), 2);
+        internal static readonly Suit Diamonds = new Suit(nameof(Diamonds), 3);
+        internal static readonly Suit Clubs = new Suit(nameof(Clubs), 4);
+
+        private Suit(string name, int value) : base(name, value) { }
+    }
+}
