b96a81059863d39ad342945b3d351c1a6369d83e
Logan Jerrow <2975388+Logan-Jerrow@users.noreply.github.com>
2020-09-04 01:40:49 -0700

    Added shuffle.

--- CardLibrary/Deck.cs
+++ CardLibrary/Deck.cs
@@ -3,16 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Text;
+using MoreLinq;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace CardLibrary
 {
-   public sealed class Deck
+    public sealed class Standard52CardDeck
     {
-        private readonly ImmutableList<Card> deck;
+        public ImmutableList<Card> Deck { get; private set; }
 
-        public Deck()
+        public Standard52CardDeck()
         {
-            deck = Card.List.ToImmutableList();
+            Deck = Card.List.ToImmutableList();
+        }
+
+        public void Shuffle()
+        {
+            List<Card> list = new List<Card>(Deck);
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                int n = list.Count;
+                while (n > 1)
+                {
+                    byte[] box = new byte[1];
+                    do provider.GetBytes(box);
+                    while (!(box[0] < n * (byte.MaxValue / n)));
+                    int k = (box[0] % n);
+                    n--;
+                    Card value = list[k];
+                    list[k] = list[n];
+                    list[n] = value;
+                }
+            }
+            Deck = list.ToImmutableList();
         }
     }
 }
